7cd14be341686881d9bc1477350e655b14f2db55
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-05-13 20:26:27 +0100

    feat: :fire: Remove JS

--- src/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.db = void 0;
-var sequelize_1 = require("sequelize");
-// Instance Sequelize SQLite3
-exports.db = new sequelize_1.Sequelize({
-    dialect: "sqlite",
-    storage: "./db/database.sqlite"
-});
